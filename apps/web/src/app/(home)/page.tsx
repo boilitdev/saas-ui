e97cf31e34bdfad4680494610c13da7c3d92ffde
Bruno Wego <brunowego@gmail.com>
2025-08-30 15:23:35 -0300

    feat: Add web app with initial files and configurations

---
+++ apps/web/src/app/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>SaaS/UI is great!</h1>
+}
---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
+
+import { fontSans } from '@/lib/fonts'
+import '@/styles/globals.css'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'ACME',
+    template: '%s — ACME',
+  }
+}
+
+type RootLayoutProps ={
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={fontSans.className}>{children}</body>
+    </html>
+  )
+}
